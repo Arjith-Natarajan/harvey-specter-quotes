193792613929326445ac15545f12de1df532a9e8
Arjith Natarajan <arjith@sigaramtech.com>
2018-07-06 22:54:33 +0530

    feat: Setup basic testing

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var harveyQuotes = require('.');
+var expect = require('chai').expect;
+
+describe('harvey-specter-quotes', () => {
+  it('should work', function () {
+    expect(true).to.be.true;
+  })
+});
